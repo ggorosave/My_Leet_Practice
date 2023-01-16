11bd2c21309e3db511ec5216a08aa14a6d44f527
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-01-16 16:33:46 -0700

    Push starter code with some personal notes

---
+++ Roman to Integer/romantoInt.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let number = 0;
+
+    charArray = Array.from(s);
+
+    // loop through the array
+    // check if index and idex + 1 .joined = one of the letter combos, then add to number
+
+    
+
+    
+
+    return number;
+};
+
+console.log(romanToInt(0));
