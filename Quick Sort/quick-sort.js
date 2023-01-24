0cdd8fd77d2286f2805c544c86db939145aa89e1
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-01-24 16:55:25 -0700

    initial commit of starter code and outline of tasks to complete for code

---
+++ Quick Sort/quick-sort.js
@@ -0,0 +1,15 @@
+const testArray = [1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92];
+
+function quickSort(array) {
+    // 1. choose a pivot (median of three - randomly choose three elements and select the median of those elements)
+    // 2. anything greater than the pivot goes on the right
+    // 3. anything less than the pivot goes on the left
+    // 4. repeat 1-3 until we are left with a single element in each list
+    // 5. then put back together; everything on the left goes infront of the pivot; eveything on the right goes behind the pivot
+    
+
+
+
+
+    return array;
+  }
