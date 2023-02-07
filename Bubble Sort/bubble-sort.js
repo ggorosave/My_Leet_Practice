d1478769705477518af6d6a67ea71c68a597c992
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-07 16:18:40 -0700

    Final solution

--- Bubble Sort/bubble-sort.js
+++ Bubble Sort/bubble-sort.js
@@ -1,24 +1,20 @@
 const testArray = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
 
 const bubbleSort = (array) => {
-
-    // const sortedArray = []
     
-    for (let i = 0; i < array.length; i++) {
-        
-        // while (array[i] > array[i + 1]) {
-        //     if (array[i] > array[i + 1]) {
-        //         let tmp = array[i];
-        //         [array[i], array[i + 1]] = [array[i + 1], tmp]
-        //     }
-        // }
+    // iterates for the length of the array minus one because the largest number should bubble to the end in the first iteration
+    for (let i = 0; i < array.length - 1; i++) {
 
-        for (const element of array) {
-            if (element > array[i]) {
-                // let tmp = array[i];
-                [element, array[i]] = [array[i], element]
+        // iterates through the array to check whether the current element is greater than the next
+        // it subracts 1 and i from each iteration beacuse the largest numbers should 'bubble' to the end with each consecutive iteration
+        for (let j = 0; j < array.length - 1 - i; j++) {
+
+            // swaps the values in the array if the value of the current element is greater than the next element
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
             }
         }
+        
     }
 
     return array;
