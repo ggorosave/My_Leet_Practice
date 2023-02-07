a791e08a774433cd2aa34dbc8dc3825f0a77ab01
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-07 15:55:01 -0700

    Second attempt

--- Bubble Sort/bubble-sort.js
+++ Bubble Sort/bubble-sort.js
@@ -2,16 +2,26 @@ const testArray = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 23
 
 const bubbleSort = (array) => {
 
+    // const sortedArray = []
+    
     for (let i = 0; i < array.length; i++) {
+        
+        // while (array[i] > array[i + 1]) {
+        //     if (array[i] > array[i + 1]) {
+        //         let tmp = array[i];
+        //         [array[i], array[i + 1]] = [array[i + 1], tmp]
+        //     }
+        // }
 
-        while (array[i] > array[i + 1]) {
-            if (array[i] > array[i + 1]) {
-                let tmp = array[i];
-                [array[i], array[i + 1]] = [array[i + 1], tmp]
+        for (const element of array) {
+            if (element > array[i]) {
+                // let tmp = array[i];
+                [element, array[i]] = [array[i], element]
             }
         }
     }
 
+    return array;
 };
 
 console.log(bubbleSort(testArray));
